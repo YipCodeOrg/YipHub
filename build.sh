c6b421e99c3d8b7bad26d8119e7a465f80f1cf99
Colm Bhandal <bhandalc@gmail.com>
2022-06-13 16:58:11 +0100

    Different ENVs and build script

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ $# -eq 0 ]; then
+    echo "No arguments provided"
+    exit 1
+fi
+
+#First arg passed in is the enivornment for which to build e.g. "dev", "local", "prod"
+env=$(echo $1 | tr [:upper:] [:lower:])
+
+if ! [[ $env =~ ^dev|prod|local ]]; then
+    echo "Invalid ENV: $env"
+    exit 1
+fi
+
+rm -r build
+mkdir -p build
+cp *.html build
+
+cp env/$env/env.js build
---
+++ env/local/env.js
@@ -0,0 +1,3 @@
+env = {
+    environment: "LOCAL"
+}
