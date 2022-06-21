c1d2964ae814818b7987cc338ee8f9ce77ffe0c0
Colm Bhandal <bhandalc@gmail.com>
2022-06-21 18:42:31 +0100

    Refactored Cognito URL
    More reusable parts now stored as ENV vars

--- env/local/env.js
+++ env/local/env.js
@@ -1,4 +1,6 @@
 env = {
     environment: "local",
-    selfOrigin: "http://localhost:8000"
+    selfOrigin: "http://localhost:8000",
+    cognitoOrigin: "https://dev-yipcode-auth.auth.us-east-1.amazoncognito.com",
+    cognitoClientId: "6q5ace1mao2cgnvt0ko89puio5"
 }
