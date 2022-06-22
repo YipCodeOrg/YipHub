3d23b96e3fc7726cbc2a00cdd2df3c86771b07f0
Colm Bhandal <bhandalc@gmail.com>
2022-06-22 16:18:29 +0100

    Stub message posting
    Posts a hello world message to front
    Stub receiver that just prints message for now

--- env/dev/env.js
+++ env/dev/env.js
@@ -1,5 +1,6 @@
 env = {
     environment: "dev",
+    yipFrontOrigin: "https://dev.yipcode.com",
     selfOrigin: "https://hub.dev.yipcode.com",
     cognitoOrigin: "https://dev-yipcode-auth.auth.us-east-1.amazoncognito.com",
     cognitoClientId: "6q5ace1mao2cgnvt0ko89puio5"
--- env/local/env.js
+++ env/local/env.js
@@ -1,5 +1,6 @@
 env = {
     environment: "local",
+    yipFrontOrigin: "http://localhost:3000",
     selfOrigin: "http://localhost:8000",
     cognitoOrigin: "https://dev-yipcode-auth.auth.us-east-1.amazoncognito.com",
     cognitoClientId: "6q5ace1mao2cgnvt0ko89puio5"
