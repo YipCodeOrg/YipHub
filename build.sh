ecb5cac46b9e8454a7e06916c9686241e906a823
Colm Bhandal <bhandalc@gmail.com>
2022-06-24 17:44:51 +0100

    Env cache for local builds
    Reading from AWS every time would be inefficient
    This allows local builds to cache the env file to be used
    The cache is ingored by Git
    User needs to manually create the cache to use it

--- build.sh
+++ build.sh
@@ -17,42 +17,57 @@ fi
 build_target_dir=build/$env
 
 echo "Building into directory: ${build_target_dir}"
-echo "Replacing content..."
 
+echo "Replacing content..."
 rm -r $build_target_dir
 mkdir -p $build_target_dir
 declare -a dirs=("api" "auth")
 for d in "${dirs[@]}"; do cp -r "$d" $build_target_dir; done
-cp env/$env/env.js favicon.ico $build_target_dir
-
+cp favicon.ico $build_target_dir
 echo "...Replaced content"
 
-aws_profile='default'
-cognito_user_pool_name='dev.yipcode.com'
-
-if [[ $env = prod ]]; then
-    aws_profile='root'
-    cognito_user_pool_name='yipcode.com'
+echo "Copying env file..."
+env_file=env/$env/env.js
+is_env_cached=false
+local_env_file=.local/env.js
+if [[ $env=='local' && -f $local_env_file ]]; then
+    echo "Locally cached env file detected. Will be used for local build."
+    is_env_cached=true
+    env_file=$local_env_file
 fi
+cp $env_file $build_target_dir
+echo "...Env file copied"
 
-echo "Retreiving & writing client ID for aws_profile ${aws_profile} and user pool ${cognito_user_pool_name}..."
+echo "Is env cached: ${is_env_cached}"
+if [ $is_env_cached == false ]; then
+    echo "No locally cached file found. Reading details from AWS."
+    aws_profile='default'
+    cognito_user_pool_name='dev.yipcode.com'
 
-user_pool_info=$(aws cognito-idp list-user-pools --max-results 1 --profile $aws_profile)
-if grep -q "$cognito_user_pool_name" <<< $user_pool_info; then
-    user_pool_id=$(aws cognito-idp list-user-pools --max-results 1 --profile $aws_profile | grep -Po '(?<="Id": ")(.+)(?=")')
-    echo "Found user pool ID: ${user_pool_id}"
-    client_info=$(aws cognito-idp list-user-pool-clients --user-pool-id $user_pool_id --profile $aws_profile --max-items 1)
-    if grep -q "$StandardClient" <<< $client_info; then
-        client_id=$(aws cognito-idp list-user-pool-clients --user-pool-id $user_pool_id --profile $aws_profile --max-items 1 | grep -Po '(?<="ClientId": ")(.+)(?=")')
-        echo "Found client ID: ${client_id}"
-        sed -i -e "s/COGNITO_CLIENT_ID_PLACEHOLDER/${client_id}/g" $build_target_dir/env.js
+    if [[ $env = prod ]]; then
+        aws_profile='root'
+        cognito_user_pool_name='yipcode.com'
+    fi
+
+    echo "Retreiving & writing client ID for aws_profile ${aws_profile} and user pool ${cognito_user_pool_name}..."
+
+    user_pool_info=$(aws cognito-idp list-user-pools --max-results 1 --profile $aws_profile)
+    if grep -q "$cognito_user_pool_name" <<< $user_pool_info; then
+        user_pool_id=$(aws cognito-idp list-user-pools --max-results 1 --profile $aws_profile | grep -Po '(?<="Id": ")(.+)(?=")')
+        echo "Found user pool ID: ${user_pool_id}"
+        client_info=$(aws cognito-idp list-user-pool-clients --user-pool-id $user_pool_id --profile $aws_profile --max-items 1)
+        if grep -q "$StandardClient" <<< $client_info; then
+            client_id=$(aws cognito-idp list-user-pool-clients --user-pool-id $user_pool_id --profile $aws_profile --max-items 1 | grep -Po '(?<="ClientId": ")(.+)(?=")')
+            echo "Found client ID: ${client_id}"
+            sed -i -e "s/COGNITO_CLIENT_ID_PLACEHOLDER/${client_id}/g" $build_target_dir/env.js
+        else
+            echo "Error: standard client not found"
+            exit 1
+        fi
     else
-        echo "Error: standard client not found"
+        echo "Error: user pool not found"
         exit 1
     fi
-else
-    echo "Error: user pool not found"
-    exit 1
-fi
 
-echo "...client ID written."
+    echo "...client ID written."
+fi
