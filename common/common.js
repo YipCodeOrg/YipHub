f3133c1e1228cd252d3b0305f24cd918705a5560
Colm Bhandal <bhandalc@gmail.com>
2022-07-07 11:42:57 +0100

    Fetch from API
    Replaced stub code with code to fetch from API
    API is hosted in cloud
    Code attaches an id_token via the Authorization header

--- common/common.js
+++ common/common.js
@@ -1,1 +1,9 @@
 validFrontRedirects = ["/app", "/app/dashboard", "/app/create"]
+
+function getIdToken(){
+    const token = localStorage.getItem("id_token")
+    if(!!token){
+        return token
+    }
+    throw new Error("ID token not found")
+}
